df93f20c76bf264fa8dbcc5907024503c4e5d50b
Gabi-Garcia <96738373+Gabi-Garcia@users.noreply.github.com>
2024-08-29 16:55:55 +0200

    Modified:index.html, main.js, style.css, Deleted: assets/footer, New file: data/products.js

--- main.js
+++ main.js
@@ -1,92 +1,97 @@
 
-/*12 productos*/
+// /*12 productos*/
 
-const products = [
-    {
-        name: 'Black Crop Tailored Jacket',
-        price: 10,
-        vendedor: 'Zara',
-        stars: 4,
-        image:'./assets/pexels-dima-valkov-6211617 1 (1).png'
-    },
-    {
-        name: 'Regular Fit Long Sleeve Top',
-        price: 20,
-        vendedor: 'Primark',
-        stars: 4,
-        image:'./assets/pexels-dima-valkov-6211621 1 (1).png'
-    },
-    {
-        name: 'Ultimate style upgrade',
-        price: 30,
-        vendedor: 'Mango',
-        stars: 3,
-        image:'./assets/excited-white-girl-bright-stylish.png'
-    },
-    {
-        name: 'Collection of stylish clothing',
-        price: 40,
-        vendedor: 'BCN',
-        stars: 4,
-        image:'./assets/pexels-dima-valkov-6402847 2 (1).png'
-    }, {
-        name: 'Textured Sunset Shirt',
-        price: 50,
-        vendedor: 'Zara',
-        stars: 5,
-        image:'./assets/pexels-dima-valkov-6503007 3 (1).png'
-    },
-    {
-        name: 'Outerwear',
-        price: 60,
-        vendedor: 'Primark',
-        stars: 5,
-        image:'./assets/vknivfu sdkvbjieurg kjvn 1.png'
-    },
-    {
-        name: 'Gray Solid Padded Jacket',
-        price: 70,
-        vendedor: 'Mango',
-        stars: 4,
-        image:'./assets/pexels-monstera-6311613 (1) 1 (1).png'
-    },
-    {
-        name: 'Tailored Jacket',
-        price: 80,
-        vendedor: 'BCN',
-        stars: 5,
-        image:'./assets/pexels-dima-valkov-6211601 1 (1).png'
-    },
-    {
-        name: 'Spread Collar Shirt',
-        price: 90,
-        vendedor: 'Zara',
-        stars: 4,
-        image:'./assets/pexels-dima-valkov-6402848 1 (1).png'
-    },
-    {
-        name: 'Printed Loose T-shirt',
-        price: 100,
-        vendedor: 'Primark',
-        stars: 5,
-        image:'./assets/pexels-dima-valkov-6211614 1 (1).png'
-    },
-    {
-        name: 'Solid Round Neck T-shirt',
-        price: 110,
-        vendedor: 'Mango',
-        stars: 5,
-        image:'./assets/pexels-monstera-6311612 3 (1).png'
-    },
-    {
-        name: 'Summer Wind Crop Shirt',
-        price: 120,
-        vendedor: 'BCN',
-        stars: 5,
-        image:'./assets/pexels-dima-valkov-6402846 1 (1).png'
-    }
-]
+// const products = [
+//     {
+//         name: 'Black Crop Tailored Jacket',
+//         price: 10,
+//         vendedor: 'Zara',
+//         stars: 4,
+//         image:'./assets/pexels-dima-valkov-6211617 1 (1).png'
+//     },
+//     {
+//         name: 'Regular Fit Long Sleeve Top',
+//         price: 20,
+//         vendedor: 'Primark',
+//         stars: 4,
+//         image:'./assets/pexels-dima-valkov-6211621 1 (1).png'
+//     },
+//     {
+//         name: 'Ultimate style upgrade',
+//         price: 30,
+//         vendedor: 'Mango',
+//         stars: 3,
+//         image:'./assets/excited-white-girl-bright-stylish.png'
+//     },
+//     {
+//         name: 'Collection of stylish clothing',
+//         price: 40,
+//         vendedor: 'BCN',
+//         stars: 4,
+//         image:'./assets/pexels-dima-valkov-6402847 2 (1).png'
+//     }, {
+//         name: 'Textured Sunset Shirt',
+//         price: 50,
+//         vendedor: 'Zara',
+//         stars: 5,
+//         image:'./assets/pexels-dima-valkov-6503007 3 (1).png'
+//     },
+//     {
+//         name: 'Outerwear',
+//         price: 60,
+//         vendedor: 'Primark',
+//         stars: 5,
+//         image:'./assets/vknivfu sdkvbjieurg kjvn 1.png'
+//     },
+//     {
+//         name: 'Gray Solid Padded Jacket',
+//         price: 70,
+//         vendedor: 'Mango',
+//         stars: 4,
+//         image:'./assets/pexels-monstera-6311613 (1) 1 (1).png'
+//     },
+//     {
+//         name: 'Tailored Jacket',
+//         price: 80,
+//         vendedor: 'BCN',
+//         stars: 5,
+//         image:'./assets/pexels-dima-valkov-6211601 1 (1).png'
+//     },
+//     {
+//         name: 'Spread Collar Shirt',
+//         price: 90,
+//         vendedor: 'Zara',
+//         stars: 4,
+//         image:'./assets/pexels-dima-valkov-6402848 1 (1).png'
+//     },
+//     {
+//         name: 'Printed Loose T-shirt',
+//         price: 100,
+//         vendedor: 'Primark',
+//         stars: 5,
+//         image:'./assets/pexels-dima-valkov-6211614 1 (1).png'
+//     },
+//     {
+//         name: 'Solid Round Neck T-shirt',
+//         price: 110,
+//         vendedor: 'Mango',
+//         stars: 5,
+//         image:'./assets/pexels-monstera-6311612 3 (1).png'
+//     },
+//     {
+//         name: 'Summer Wind Crop Shirt',
+//         price: 120,
+//         vendedor: 'BCN',
+//         stars: 5,
+//         image:'./assets/pexels-dima-valkov-6402846 1 (1).png'
+//     }
+// ]
  
+
+import  { products } from "./data/products.js";
+
+
+let i= 0;
 
 const productsCards = document.querySelector('#products-cards');
 const getProductTemplate = /*(name, price, seller, stars, image)*/ (product) => {
